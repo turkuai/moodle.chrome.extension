3c07aafdae8df69bb8343a83607a43d70063acc1
Bogdan Udrescu <bogdan.udrescu@gmail.com>
2023-08-23 11:18:34 +0300

    testing scroll

--- tai.moodle.chrome.extension/extension.js
+++ tai.moodle.chrome.extension/extension.js
@@ -1,33 +1,46 @@
+addEventListener("load", event => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const scrollPosition = urlParams.get("scrollTo")
+    
+    if (scrollPosition) {
+        window.scrollTo(0, scrollPosition);
+        console.log("scrollPosition:", scrollPosition);
+    }
+});
+
 const mainNavigation = document.getElementById("main-navigation");
 const mainMenu = getElementByClass("main-menu", mainNavigation);
 
-console.log("mainMenu", mainMenu);
-console.log("mainMenu.tagName", mainMenu.tagName.toLowerCase());
-
-const pageHeaderRight = getElementByClass("page-header-right");
-if (pageHeaderRight) {
-    if (pageHeaderRight.children.length > 2) {
-        addMenuItem(pageHeaderRight.children[2], mainMenu);
+if (mainMenu) {
+    console.log("mainMenu", mainMenu);
+    console.log("mainMenu.tagName", mainMenu.tagName.toLowerCase());
+    
+    const pageHeaderRight = getElementByClass("page-header-right");
+    if (pageHeaderRight) {
+        if (pageHeaderRight.children.length > 2) {
+            const editItem = pageHeaderRight.children[2];
+            // Edit item.
+            insertMenuItemAsFirst(editItem, mainMenu);
+        }
+        if (pageHeaderRight.children.length > 1) {
+            insertMenuItemAsFirst(pageHeaderRight.children[1], mainMenu);
+        }
     }
-    if (pageHeaderRight.children.length > 1) {
-        addMenuItem(pageHeaderRight.children[1], mainMenu);
+    
+    const fixedBar = getElementByClass("fixed-bar");
+    if (fixedBar && fixedBar.children.length) {
+        fixedBar.classList.add("fixed-bar-menu");
+    
+        const index = fixedBar.children.length > 1 ? 1 : 0;
+    
+        configureToggleElement(fixedBar.children[index].children[0], "eye10", "Piilota lohkot");
+        configureToggleElement(fixedBar.children[index].children[1], "eye9", "Näytä lohkot");
+    
+        if (index > 0) {
+            fixedBar.removeChild(fixedBar.children[index - 1]);
+        }
+        insertMenuItemAsFirst(fixedBar, mainMenu)
     }
-    // addMenuItem(pageHeaderRight, mainMenu);
-}
-
-const fixedBar = getElementByClass("fixed-bar");
-if (fixedBar && fixedBar.children.length) {
-    fixedBar.classList.add("fixed-bar-menu");
-
-    const index = fixedBar.children.length > 1 ? 1 : 0;
-
-    configureToggleElement(fixedBar.children[index].children[0], "eye10", "Piilota lohkot");
-    configureToggleElement(fixedBar.children[index].children[1], "eye9", "Näytä lohkot");
-
-    if (index > 0) {
-        fixedBar.removeChild(fixedBar.children[index - 1]);
-    }
-    addMenuItem(fixedBar, mainMenu)
 }
 
 function configureToggleElement(element, className, tooltip) {
@@ -37,22 +50,17 @@ function configureToggleElement(element, className, tooltip) {
     element.setAttribute("data-original-title", tooltip);
 
     const i = document.createElement("i");
-    // i.setAttribute("src", chrome.runtime.getURL("svg/eye-9.svg"));
     i.classList.add(className);
 
     element.appendChild(i);
 }
 
-function addMenuItem(element, menuElement) {
+function insertMenuItemAsFirst(element, menuElement) {
     if (menuElement && menuElement.tagName.toLowerCase() == "ul") {
-
-        console.log("menuElement.children", menuElement.children);
 
         if (menuElement.children.length) {
             const item = document.createElement("li");
             item.appendChild(element)
-
-            console.log("item", item);
 
             menuElement.insertBefore(item, menuElement.children[0]);
         }
